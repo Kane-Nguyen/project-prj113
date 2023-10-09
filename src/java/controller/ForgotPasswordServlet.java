075d79fa70d29dbd301042b5bd871141121dd495
Kane <khanh@kdigi.vn>
2023-10-09 14:39:14 +0700

    commit

--- src/java/controller/ForgotPasswordServlet.java
+++ src/java/controller/ForgotPasswordServlet.java
@@ -11,12 +11,6 @@ import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.http.HttpSession;
-import java.util.Random;
-import javax.mail.*;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
-import java.util.Properties;
 
 /**
  *
@@ -65,46 +59,7 @@ public class ForgotPasswordServlet extends HttpServlet {
             throws ServletException, IOException {
         request.getRequestDispatcher("/forgotPassword.jsp").forward(request, response);
     }
-     public int generateRandomCode() {
-        Random random = new Random();
-        return 100000 + random.nextInt(900000); // This will generate a random 6-digit number
-    }
-      private void sendEmail(String toEmail, int code) {
-        final String username = "kanekha2@gmail.com";
-        final String password = "Kha123abcd";
-
-        Properties prop = new Properties();
-        prop.put("mail.smtp.host", "smtp.gmail.com");
-        prop.put("mail.smtp.port", "25");
-        prop.put("mail.smtp.auth", "true");
-        prop.put("mail.smtp.starttls.enable", "true");
-
-        Session session = Session.getInstance(prop,
-                new javax.mail.Authenticator() {
-                    @Override
-                    protected PasswordAuthentication getPasswordAuthentication() {
-                        return new PasswordAuthentication(username, password);
-                    }
-                });
-
-        try {
-            Message message = new MimeMessage(session);
-            message.setFrom(new InternetAddress(username));
-            message.setRecipients(
-                    Message.RecipientType.TO,
-                    InternetAddress.parse(toEmail)
-            );
-            message.setSubject("Your verification code");
-            message.setText("Your verification code is: " + code);
-
-            Transport.send(message);
-
-            System.out.println("Email sent successfully");
-
-        } catch (MessagingException e) {
-            e.printStackTrace();
-        }
-    }
+   
 
     /**
      * Handles the HTTP <code>POST</code> method.
@@ -113,26 +68,6 @@ public class ForgotPasswordServlet extends HttpServlet {
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
-     */
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-        String email = request.getParameter("email");
-    int code = generateRandomCode(); // Implement this function to generate a 6-digit code
-    sendEmail(email, code); // Implement this function to send the code via email
-
-    // Store the code in the session
-    HttpSession session = request.getSession();
-    session.setAttribute("code", code);
-
-    // Redirect to the enterCode page
-    response.sendRedirect("enterCode.jsp");
-    }
-
-    /**
-     * Returns a short description of the servlet.
-     *
-     * @return a String containing servlet description
      */
     @Override
     public String getServletInfo() {
